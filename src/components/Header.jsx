dee280b72aebdf28a95d6bc6643dea42a7bfbfdd
Abhay Tomar <tomarabhay767@gmail.com>
2025-07-19 16:21:22 +0530

    make the header paragraph text dynamic & imported the img src as value

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-
+import reactImg from '../assets/react-core-concepts.png'
 export default function Header() {
+
+  const reactDescriptions = ["Fundamental","Core","Crucial"];
+  const getRandomInt =(max)=>{
+      return Math.floor(Math.random()*(max+1));
+  }
+  const description = reactDescriptions[getRandomInt(2)]
   return (
     <>
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {description} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
