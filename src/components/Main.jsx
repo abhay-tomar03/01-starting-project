d35e0af266241d6f2347382734e3d8282f1cc18d
Abhay Tomar <tomarabhay767@gmail.com>
2025-07-19 17:26:02 +0530

    Splitted the main component , created separate listitems & tabbutton components and passed props.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,29 @@
+import ListItems from "./ListItems";
+import {CORE_CONCEPTS} from '../data'
+import TabButtons from "./TabButtons";
 export default function Main() {
   return (
     <>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <ListItems {...CORE_CONCEPTS[0]} />
+            <ListItems {...CORE_CONCEPTS[1]} />
+            <ListItems {...CORE_CONCEPTS[2]} />
+            <ListItems {...CORE_CONCEPTS[3]} />
+
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+            <menu>
+              <TabButtons>Components</TabButtons>
+              <TabButtons>JSX</TabButtons>
+              <TabButtons>Props</TabButtons>
+              <TabButtons>States</TabButtons>
+            </menu>
+        </section>
       </main>
     </>
   );
