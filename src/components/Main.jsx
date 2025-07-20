254dc88d8f0d1f79ab4c055e5bbb715b11ef4801
Abhay Tomar <tomarabhay767@gmail.com>
2025-07-20 20:12:07 +0530

    Displyaed Conditional Unordered list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -15,10 +15,16 @@ export default function Main() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <ListItems {...CORE_CONCEPTS[0]} />
+
+            {/* dyanmically displaying the list */}
+
+            {CORE_CONCEPTS.map((conceptItem,key)=><ListItems key={conceptItem.title} {...conceptItem} />   )}
+
+            {/* This data is displayed not dynamically so in the data.js if any of the data is missing it will throw an error */}
+            {/* <ListItems {...CORE_CONCEPTS[0]} />
             <ListItems {...CORE_CONCEPTS[1]} />
             <ListItems {...CORE_CONCEPTS[2]} />
-            <ListItems {...CORE_CONCEPTS[3]} />
+            <ListItems {...CORE_CONCEPTS[3]} /> */}
 
           </ul>
         </section>
